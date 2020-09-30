39d6f5cc522e86e9c1d64a475e32283a898cf89d
Kain <staride@naver.com>
2020-10-01 01:43:51 +0900

    Web Repo init

--- proj/frontend-spring/README.md
+++ proj/frontend-spring/README.md
@@ -1,2 +1,13 @@
 # Vue-Spring Project
-이곳은 Front-End 와 Spring 관련 코드가 기록되는 저장소 입니다.  
+해당 프로젝트는 다음 버전으로 초기화 되었습니다.
+
+backend
+  - Spring boot : 2.3.4
+  - jdk : 11
+  
+frontend
+  - Vue Cli : @vue/cli 4.5.6
+  - Vuetify : vue-cli-plugin-vuetify@2.0.7
+  - vue-router : vue-router@3.4.5
+  - axios : axios@0.20.0
+
---
+++ proj/frontend-spring/stockWeb/frontend/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <v-btn @click="testClickEvent">Test</v-btn>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Home',
+  methods: {
+    testClickEvent: function () {
+      axios.get('http://localhost:8000/test').then(res => {
+        if (res.status === 200) {
+          alert(res.data)
+        } else {
+          console.log('status : ' + res.status + ', data : ' + res.data)
+        }
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
+}
+</script>
